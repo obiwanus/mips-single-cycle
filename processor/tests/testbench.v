d435de6b5844f1c53680194cb3ca2758bf462519
Ivan Ivanov <ivan@ivanovs.info>
2017-07-17 06:09:06 +1200

    balmn test

--- processor/tests/testbench.v
+++ processor/tests/testbench.v
@@ -17,27 +17,28 @@ module testbench;
     test_control control();
 
     // Processor tests
-    test_fefe fefe();
-    test_memset memset();
+    // test_fefe fefe();
+    // test_memset memset();
 
     // ALU instruction tests
-    test_addi addi();
-    test_add add();
-    test_sub sub();
-    test_and and_t();
-    test_or or_t();
-    test_nor nor_t();
-    test_andi andi();
-    test_ori ori();
-    test_sll sll();
-    test_srl srl();
-    test_slt slt();
+    // test_addi addi();
+    // test_add add();
+    // test_sub sub();
+    // test_and and_t();
+    // test_or or_t();
+    // test_nor nor_t();
+    // test_andi andi();
+    // test_ori ori();
+    // test_sll sll();
+    // test_srl srl();
+    // test_slt slt();
 
     // Jump tests
-    test_j j();
-    test_jr jr();
+    // test_j j();
+    // test_jr jr();
 
     // Branch tests
-    test_bne bne();
+    // test_bne bne();
+    test_balmn balmn();
 
 endmodule
