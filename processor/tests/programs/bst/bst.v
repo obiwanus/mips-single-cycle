cd0ef8b429dde2b234599c3458c669201553f26e
Ivan Ivanov <ivan@ivanovs.info>
2017-07-20 08:04:57 +1200

    Binary search tree + sort2list work

--- processor/tests/programs/bst/bst.v
+++ processor/tests/programs/bst/bst.v
@@ -8,66 +8,31 @@ module test_bst;
     initial begin
         $readmemb("tests/programs/bst/bst_imem.dat", CPU.IFU.imemory.storage.bytes);
 
-        repeat(1000) @(posedge CPU.clk);
-
-        // `assertEq(CPU.dmemory.bytes[0], 8'h2)
-        // `assertEq(CPU.dmemory.bytes[1], 8'h0)
-        // `assertEq(CPU.dmemory.bytes[2], 8'h0)
-        // `assertEq(CPU.dmemory.bytes[3], 8'h0)
-
-        // `assertEq(CPU.dmemory.bytes[4], 8'hf6)
-        // `assertEq(CPU.dmemory.bytes[5], 8'hff)
-        // `assertEq(CPU.dmemory.bytes[6], 8'hff)
-        // `assertEq(CPU.dmemory.bytes[7], 8'hff)
-
-        // `assertEq(CPU.dmemory.bytes[8],  8'h9)
-        // `assertEq(CPU.dmemory.bytes[9],  8'h0)
-        // `assertEq(CPU.dmemory.bytes[10], 8'h0)
-        // `assertEq(CPU.dmemory.bytes[11], 8'h0)
-
-        // `assertEq(CPU.dmemory.bytes[12], 8'h3)
-        // `assertEq(CPU.dmemory.bytes[13], 8'h0)
-        // `assertEq(CPU.dmemory.bytes[14], 8'h0)
-        // `assertEq(CPU.dmemory.bytes[15], 8'h0)
-
-        // `assertEq(CPU.dmemory.bytes[16], 8'hf9)
-        // `assertEq(CPU.dmemory.bytes[17], 8'hff)
-        // `assertEq(CPU.dmemory.bytes[18], 8'hff)
-        // `assertEq(CPU.dmemory.bytes[19], 8'hff)
-
-        // `assertEq(CPU.dmemory.bytes[20], 8'h0)
-        // `assertEq(CPU.dmemory.bytes[21], 8'h0)
-        // `assertEq(CPU.dmemory.bytes[22], 8'h0)
-        // `assertEq(CPU.dmemory.bytes[23], 8'h0)
-
-        // `assertEq(CPU.dmemory.bytes[24], 8'h0c)
-        // `assertEq(CPU.dmemory.bytes[25], 8'h00)
-        // `assertEq(CPU.dmemory.bytes[26], 8'h00)
-        // `assertEq(CPU.dmemory.bytes[27], 8'h00)
-
-        // // Check min and max
-        // `assertEq(CPU.registers.registers[`REG_S1], -32'd300)
-        // `assertEq(CPU.registers.registers[`REG_S2], 32'd999)
-
-        `assertEq(CPU.dmemory.bytes[0], 8'h2)
-        `assertEq(CPU.dmemory.bytes[1], 8'h0)
-        `assertEq(CPU.dmemory.bytes[2], 8'h0)
-        `assertEq(CPU.dmemory.bytes[3], 8'h0)
-
-        `assertEq(CPU.dmemory.bytes[4], 8'hf6)
-        `assertEq(CPU.dmemory.bytes[5], 8'hff)
-        `assertEq(CPU.dmemory.bytes[6], 8'hff)
-        `assertEq(CPU.dmemory.bytes[7], 8'hff)
-
-        `assertEq(CPU.dmemory.bytes[8],  8'h9)
-        `assertEq(CPU.dmemory.bytes[9],  8'h0)
-        `assertEq(CPU.dmemory.bytes[10], 8'h0)
-        `assertEq(CPU.dmemory.bytes[11], 8'h0)
-
+        repeat(1120) @(posedge CPU.clk);
 
         // Check min and max
-        `assertEq(CPU.registers.registers[`REG_S1], -32'd300)
-        `assertEq(CPU.registers.registers[`REG_S2], 32'd999)
+        `assertEq(CPU.dmemory.bytes[500], 8'hd4)
+        `assertEq(CPU.dmemory.bytes[501], 8'hfe)
+        `assertEq(CPU.dmemory.bytes[502], 8'hff)
+        `assertEq(CPU.dmemory.bytes[503], 8'hff)
+
+        `assertEq(CPU.dmemory.bytes[512], 8'he7)
+        `assertEq(CPU.dmemory.bytes[513], 8'h03)
+        `assertEq(CPU.dmemory.bytes[514], 8'h0)
+        `assertEq(CPU.dmemory.bytes[515], 8'h0)
+
+
+        // Check sorted list
+        `assertEq(CPU.dmemory.debug_words[0], -32'd300)
+        `assertEq(CPU.dmemory.debug_words[1], -32'd10)
+        `assertEq(CPU.dmemory.debug_words[2], -32'd7)
+        `assertEq(CPU.dmemory.debug_words[3], 32'd0)
+        `assertEq(CPU.dmemory.debug_words[4], 32'd2)
+        `assertEq(CPU.dmemory.debug_words[5], 32'd3)
+        `assertEq(CPU.dmemory.debug_words[6], 32'd9)
+        `assertEq(CPU.dmemory.debug_words[7], 32'd12)
+        `assertEq(CPU.dmemory.debug_words[8], 32'd18)
+        `assertEq(CPU.dmemory.debug_words[9], 32'd999)
 
         `printResults
     end
--- processor/tests/testbench.v
+++ processor/tests/testbench.v
@@ -25,44 +25,44 @@ module testbench;
     test_sw sw();
     test_lw lw();
 
-    // // ALU instruction tests
-    // test_addi addi();
-    // test_add add();
-    // test_sub sub();
-    // test_and and_t();
-    // test_or or_t();
-    // test_nor nor_t();
-    // test_andi andi();
-    // test_ori ori();
-    // test_sll sll();
-    // test_srl srl();
-    // test_slt slt();
+    // ALU instruction tests
+    test_addi addi();
+    test_add add();
+    test_sub sub();
+    test_and and_t();
+    test_or or_t();
+    test_nor nor_t();
+    test_andi andi();
+    test_ori ori();
+    test_sll sll();
+    test_srl srl();
+    test_slt slt();
 
-    // // Jump tests
-    // test_j j();
-    // test_jr jr();
-    // test_jm jm();
-    // test_jpc jpc();
-    // test_jalr jal();
-    // test_jalr jalpc();
-    // test_jalm jalm();
+    // Jump tests
+    test_j j();
+    test_jr jr();
+    test_jm jm();
+    test_jpc jpc();
+    test_jalr jal();
+    test_jalr jalpc();
+    test_jalm jalm();
 
-    // // Branch tests
-    // test_bne bne();
-    // test_bneal bneal();
-    // test_beq beq();
-    // test_beqal beqal();
-    // test_balmn balmn();
-    // test_balmz balmz();
-    // test_balrn balrn();
-    // test_balrz balrz();
-    // test_brn brn();
-    // test_brz brz();
-    // test_bmn bmn();
-    // test_bmn bmz();
-    // test_baln baln();
-    // test_balz balz();
-    // test_bn bn();
-    // test_bz bz();
+    // Branch tests
+    test_bne bne();
+    test_bneal bneal();
+    test_beq beq();
+    test_beqal beqal();
+    test_balmn balmn();
+    test_balmz balmz();
+    test_balrn balrn();
+    test_balrz balrz();
+    test_brn brn();
+    test_brz brz();
+    test_bmn bmn();
+    test_bmn bmz();
+    test_baln baln();
+    test_balz balz();
+    test_bn bn();
+    test_bz bz();
 
 endmodule
